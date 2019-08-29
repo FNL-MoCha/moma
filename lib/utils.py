9454c0ff12368d7ad1259bad455262a7035a6c24
Dave Sims <misc@lamneth.net>
2019-08-29 10:39:31 -0400

    Can change color output in exit now.
    Added arg to method to allow one to output a different color for the
    message. This is helpful when invoking for errors rather than just
    normal exit routines.

--- lib/utils.py
+++ lib/utils.py
@@ -8,19 +8,20 @@ import datetime
 from termcolor import cprint
 
 
-def __exit__(line=None, msg=None):
+def __exit__(line=None, msg=None, color=None):
     """
     Exit, outputting a colored, formatted, line indicating where we stopped in
     the script with an optional message telling us why we stopped.  Useful for
     debugging and writing new code.
     """
     filename, lineno, function = inspect.stack()[1][1:4]
+    outcolor = 'on_green' if color is None else 'on_%s' % color
 
     line = lineno if line is None else line
     output = ('Script "{}" stopped in `{}()` at line: {} with message: '
             '"{}".'.format(os.path.basename(filename), function, line, msg))
     sys.stderr.write('\n')
-    cprint(output, "white", 'on_green', attrs=['bold'], file=sys.stderr)
+    cprint(output, "white", outcolor, attrs=['bold'], file=sys.stderr)
     sys.exit()
 
 def today():
