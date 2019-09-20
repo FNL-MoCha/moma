ed9dff632254611f14c75957c319be273cf5c907
Dave Sims <misc@lamneth.net>
2019-09-20 14:05:31 -0400

    Finished draft, working version of the aa rules. Now onto the functional rules.

--- resource/NonHotspotRules.pm
+++ resource/NonHotspotRules.pm
@@ -13,7 +13,7 @@ use Carp;
 use JSON;
 use Data::Dump;  # TODO: Go away!
 
-our $VERSION='0.2.091219';
+our $VERSION='0.3.091219';
 
 sub new {
     my $class = shift;
@@ -46,97 +46,98 @@ sub check_variant {
     # TODO: Do we want to use this as the main method entry point, which will
     # then branch out to check by aa, check by exon, and check by raw function?
     my ($self, $gene, $aa_start, $aa_end, $function, $exon) = @_;
-
-
-    return $self->codon_in_range($gene, $aa_start, $aa_end, $function, $exon);
-}
-
-sub codon_in_range {
-    ############################################################################
-    # XXX TODO:
-    #     - This method seems to work well.  See if we can simplify it further,
-    #       along with cleaning up some redundancy (especially in the terms to be
-    #       returned.
-    #
-    #     - Figure out where to consider the functional effect. We're already
-    #       parsing rules here, so maybe I ougth to handle it here rather than
-    #       waiting for later and having to re-run.
-    #
-    #     - What do we do with a null result? Need to figure out how we want to
-    #       handle those cases.
-    ############################################################################
-    
-    # Run the rules, returning either the category, oncogenicity, and effect, or
-    # a list of empty strings if no match.
-    # TODO: Figure out exactly what we want in this args list.
-    my ($self, $gene, $aa_start, $aa_end, $function, $exon) = @_;
     my ($category, $oncogenicity, $effect) = ('', '', '');
 
     if (exists $self->{'rules'}{$gene}) {
-        while (my ($index, $rule) = each @{$self->{'rules'}{$gene}}) {
-            if ($rule->{'aa_start'} or $rule->{'aa_end'}) {
-                # If only given a start bound or end bound, need to set the end
-                # bound to a very high number or start bound to 0 (respectively)
-                # just to have values for comparison.
-                $rule->{'aa_start'} = '>0' unless $rule->{'aa_start'};
-                $rule->{'aa_end'}   = '<100000' unless $rule->{'aa_end'};
+        # Run codon test.
+        my $selected_rule = $self->codon_in_range($aa_start, $aa_end, 
+            $self->{'rules'}{$gene});
+        dd $selected_rule;
+        exit;
+        ## Run function test.
+        # $self->check_function();
+        ## Run exon test.
+        # $self->check_exon();
+    }
+    return $category, $oncogenicity, $effect;
+}
 
-                if ($aa_start == $aa_end) {
-                    # We have a substitution call; only need to check once.
-                    if ($self->__comp($aa_start, $rule->{'aa_start'}) 
-                        && $self->__comp($aa_start, $rule->{'aa_end'})) {
-                        # TODO: Check func before deciding what to report.
-                        
-                        if ($self->check_function($function, $rule->{'function'})) {
-                            ($category, $oncogenicity, $effect) = 
-                                @{$rule}{qw(category oncogenicity effect)};
-                        }
-                    }
-                } else {
-                    # We have an indel, and need to check the range.
-                    my ($start_in_range, $end_in_range);
-                    $start_in_range = $self->__comp($aa_start, $rule->{'aa_start'}) 
-                        && $self->__comp($aa_start, $rule->{'aa_end'});
+sub codon_in_range {
+    # Check to see if the variant is within an amino acid range, and return the
+    # rule that matches (if one does!), which we will then check to see if the
+    # rest of the conditions match.
+    my ($self, $aa_start, $aa_end, $gene_rules) = @_;
+    
+    while (my ($index, $rule) = each @$gene_rules) {
+        if ($rule->{'aa_start'} or $rule->{'aa_end'}) {
+            # If only given a start bound or end bound, need to set the end
+            # bound to a very high number or start bound to 0 (respectively)
+            # just to have values for comparison.
+            $rule->{'aa_start'} = '>0' unless $rule->{'aa_start'};
+            $rule->{'aa_end'}   = '<100000' unless $rule->{'aa_end'};
+                
+            if ($aa_start == $aa_end) {
+                # We have a substitution call; only need to check once.
+                if ($self->__comp($aa_start, $rule->{'aa_start'}) 
+                    && $self->__comp($aa_start, $rule->{'aa_end'})) {
+                    return $rule;
+                }
+            } else {
+                # We have an indel, and need to check the range.
+                my ($start_in_range, $end_in_range);
+                $start_in_range = $self->__comp($aa_start, $rule->{'aa_start'}) 
+                    && $self->__comp($aa_start, $rule->{'aa_end'});
 
-                    $end_in_range = $self->__comp($aa_end, $rule->{'aa_start'})
-                        && $self->__comp($aa_end, $rule->{'aa_end'});
+                $end_in_range = $self->__comp($aa_end, $rule->{'aa_start'})
+                    && $self->__comp($aa_end, $rule->{'aa_end'});
 
-                    if ($start_in_range && $end_in_range) {
-                        # The whole indel within one rule. Return match data.
-                        # TODO: Check func before deciding what to report.
-                        return @{$rule}{qw(category oncogenicity effect)};
-                    }
-                    elsif ($start_in_range || $end_in_range) {
-                        # Only partial match to one rule. Check other rule and
-                        # report either partial to initial rule or partial to
-                        # both rules (assuming we only have 2 rules per gene).
-                        $oncogenicity = "Possibly Oncogenic";
+                if ($start_in_range && $end_in_range) {
+                    return $rule;
+                }
+                elsif ($start_in_range || $end_in_range) {
+                    # Only partial match to one rule. Check other rule and
+                    # report either partial to initial rule or partial to
+                    # both rules depending on what matches.
+                    if (($index+1) < @$gene_rules) {
+                        my $next_rule = $gene_rules->[$index+1];
+                        if ($start_in_range) {
+                            if ($self->__comp($aa_end, $next_rule->{'aa_end'})) {
+                                my $new_category = sprintf("(Partial) %s/%s",
+                                    $rule->{'category'}, 
+                                    $next_rule->{'category'});
+                                # Functional rules should be the same, but just
+                                # in case, get a union of the two and use that.
+                                my %union = ();
+                                for (
+                                    @{$rule->{'function'}}, 
+                                    @{$next_rule->{'function'}}
+                                ) { $union{$_}++ };
 
-                        if (($index+1) < @{$self->{'rules'}{$gene}}) {
-                            my $next_rule = $self->{'rules'}{$gene}[$index+1];
-                            if ($start_in_range) {
-                                if ($self->__comp($aa_end, $next_rule->{'aa_end'})) {
-                                    $category = sprintf("(Partial) %s/%s",
-                                        $rule->{'category'}, 
-                                        $next_rule->{'category'});
-                                    $effect = "Unknown";
-                                    # TODO: check func before deciding what to
-                                    # report.
-                                    return $category, $oncogenicity, $effect;
-                                }
+                                # Create a new rule to parse downstream.  
+                                my $new_rule = {
+                                    "exon" => [],
+                                    "aa_start" => "$rule->{'aa_start'}&$next_rule->{'aa_start'}",
+                                    "aa_end" => "$rule->{'aa_end'}&$next_rule->{'aa_end'}",
+                                    "function" => [keys %union],
+                                    "category" => $new_category,
+                                    "oncogenicity" => "Possibly Oncogenic",
+                                    "effect" => "Unknown"
+                                };
+                                return $new_rule;
                             }
                         }
-                        # We are only partially within one of the two rules.
-                        $category = "(Partial) $rule->{'category'}";
-                        $effect = "(Possible) $rule->{'effect'}";
-                        # TODO: Check func before deciding what to report.
-                        return $category, $oncogenicity, $effect;
                     }
+
+                    # We are only partially within one of the two rules.
+                    $rule->{'category'} = "(Partial) $rule->{'category'}";
+                    $rule->{'effect'} = "(Possible) $rule->{'effect'}";
+                    return $rule;
                 }
             }
         }
     }
-    return $category, $oncogenicity, $effect;
+    # If we got here, then no rules match, and we'll return False.
+    return 0;
 }
 
 sub check_function {
