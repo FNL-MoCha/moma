7f742b50d2859df1871ee78ff46a2b603b5255e4
Dave Sims <misc@lamneth.net>
2020-03-27 15:53:29 -0400

    Updated log output.

--- run_moma_pipeline.py
+++ run_moma_pipeline.py
@@ -378,8 +378,19 @@ def run_var_sig(vcf, source, outfile):
     status = run(cmd, 'Calculating Ts/Tv ratio, deamination score, and '
         'generating SBS-6 data.', ret_data=True, silent=not verbose)
     log.write_log('info', "Results from SBS script:")
+
     for i in status[2:]:
         log.write_log(None, i)
+
+    tstv = status[8].split()[1]
+    deam = status[9].split()[2]
+
+    if float(deam) > 1.7:
+        log.write_log('warn', f'Deamination score is high ({deam}). Sample may '
+                'be damaged.')
+    if float(tstv) > 4:
+        log.write_log('warn', f'Ts/Tv ratio is high ({tstv}). Sample may '
+            'be damaged.')
 
 def oncokb_annotate(annovar_file, source, popfreq):
     """
@@ -771,6 +782,8 @@ def main(vcf, data_source, sample_name, genes, popfreq, get_cnvs, cu, cl,
         sample_name = get_name_from_vcf(vcf)
 
     # Set up the logger.
+    if verbose:
+        debug = True
     level = 'debug' if debug else 'error'
     logfile = 'moma_reporter_{}_{}.log'.format(sample_name, utils.today())
 
